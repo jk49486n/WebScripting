852414a6ac9f01ffcbeca98d5667c55b2e463561
Jackie Kosky <jk49486n@pace.edu>
2017-02-21 13:03:03 -0500

    Added form to file

--- myphp.php
+++ myphp.php
@@ -5,7 +5,33 @@
 	</head>
 
 	<body>
+		
+				<form action="myphp.php" method="post">
 
+		<table border="0">
+
+		<tr>
+			<td>First Name</td>
+			<td align="center"><input type="text" name="firstname" size="30" /> </td>
+		</tr>
+
+		<tr>
+			<td>Last Name</td>
+			<td align="center"><input type="text" name="lastname" size="30" /></td>
+		</tr>
+
+		<tr>
+			<td>Student ID</td>
+			<td align="center"><input type="text" name="studentid" size="30" /></td>
+		</tr>
+
+		<tr>
+		<td colspan="2" align="center"><input type="submit" value="Submit"/></td>
+		</tr>
+
+		</table>
+		</form>
+		
 		<?php
 
 		echo "<p>Data processed</> </br>";
